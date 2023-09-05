b6a55526f51bea28aab8186b90afe2c521505d05
EugeneGpil <gpilligrim@gmail.com>
2023-09-05 04:24:04 +0000

    1

--- routerDummyExample.go
+++ routerDummyExample.go
@@ -6,18 +6,18 @@ import (
 	"github.com/EugeneGpil/router"
 )
 
+var method = http.MethodGet
+var url = "hello/world"
+var name = "hello.world"
+
+var handler = func(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Hello World!\n"))
+}
+
+var mux = http.NewServeMux()
+
 func main() {
-	method := http.MethodGet
-	url := "hello/world"
-	name := "hello.world"
-
-	handler := func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello World!\n"))
-	}
-
 	router.AddRoute(method, url, handler, name)
-
-	mux := http.NewServeMux()
 
 	router.DefineRoutes(mux)
 
